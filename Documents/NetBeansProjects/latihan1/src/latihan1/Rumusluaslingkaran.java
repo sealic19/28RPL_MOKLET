b50187af7a5fe18ef8f6b5b06ab65755985c4e6d
Gagassurya19 <gagassuryalaksana04@gmail.com>
2019-08-07 18:13:07 +0700

    Coded by @Gagassurya19 Ver 1.0

---
+++ Documents/NetBeansProjects/latihan1/src/latihan1/Rumusluaslingkaran.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+public class Rumusluaslingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            
+        //deklarasi
+        double pi, r, luas;
+        
+        //deskripsi
+        r = 10.0;
+        pi = 3.14;
+        
+        //proses
+        luas = pi * r * r;
+        
+        //output
+        System.out.print("Luas Lingkaran = ");
+        System.out.println(luas);
+    }  
+            
+    
+}

56388423971866f1fb06d0ee3906a87f50562302
sealic19 <Gagassuryalaksana04@gmail.com>
2019-08-29 08:10:34 +0700

    Update tugas2.java

--- src/sequential/tugas2.java
+++ src/sequential/tugas2.java
@@ -1,9 +1,10 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sequential;
+package sequen;
 import java.util.Scanner;
 /**
  *
@@ -18,6 +19,20 @@ public class tugas2 {
             System.out.print(data[i]+"\t");
         }
         
+        System.out.println("Rata - rata nilai array: ");
+        double x = 0;
+        for (double y : data) {
+            x = x + y;
+        } 
+        double rata = x/data.length;
+        System.out.println("nilai rata -rata: "+rata);
         
+        System.out.print("Nilai yang diatas rata - rata: ");
+        for (int z = 0; z < data.length; z++) {
+            if (data[z]>rata) {
+                System.out.print(data[z]+" ");
+            }
+        }
+        System.out.println("\n");
     }
 }
